06ee64b84a289474664487c411d73e0387d1c225
WebKing <isaac@afrocenchix.com>
2023-10-02 10:43:32 +0100

    Mobile menu completed

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,22 +3,18 @@ import { useState } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import "./Header.css";
 import logo from "../../assets/img/logo.jpg";
 import { BiMenu } from "react-icons/bi";
 
 export default function Header() {
-  const [visible, setVisible] = useState(false);
+  const [collapse, setCollapse] = useState(false);
   const pathname = usePathname();
-  const toggleHamburger = () => {
-    setVisible(!visible);
-  };
 
   return (
-    <header>
+    <header className="flex flex-row justify-between z-0">
       <Link href="/">
         <Image
-          className="logo"
+          className="logo rounded-full outline outline-accent"
           src={logo}
           width={40}
           height={40}
@@ -27,18 +23,64 @@ export default function Header() {
           priority
         />
       </Link>
-      <nav>
-        <div className="hamburger" onClick={toggleHamburger}>
-          <BiMenu />
-        </div>
-        <ul className={`menu ${visible ? "menu-show" : "menu-hide"}`}>
-          <li className={`${pathname === "/" ? "active" : ""}`}>
+      <nav className="z-0">
+        <section className="mobile-menu flex lg:hidden">
+          <div
+            className="space-y-1.5 absolute right-0 top-0 py-8 px-6"
+            onClick={() => {
+              setCollapse(true);
+            }}
+          >
+            <span className="block h-0.5 w-6 bg-accent"></span>
+            <span className="block h-0.5 w-6 bg-accent"></span>
+            <span className="block h-0.5 w-6 bg-accent"></span>
+          </div>
+          <div
+            className={`z-40 py-4 px-8 right-0 top-0 absolute h-screen bg-accent ${
+              collapse ? null : "collapse"
+            }`}
+          >
+            <div
+              className="absolute h-1.5 w-6 right-0 top-0 py-0 px-8"
+              onClick={() => {
+                setCollapse(false);
+              }}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="space-y-4 mt-12">
+              <li className="rounded-md hover:bg-accent p-0">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="rounded-md hover:bg-accent p-0">
+                <Link href="/projects">Projects</Link>
+              </li>
+              <li className="rounded-md hover:bg-accent p-0">
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <ul className="hidden gap-8 lg:flex">
+          <li className="rounded-md hover:bg-accent p-0">
             <Link href="/">Home</Link>
           </li>
-          <li className={`${pathname === "/projects" ? "active" : ""}`}>
+          <li className="rounded-md hover:bg-accent p-0">
             <Link href="/projects">Projects</Link>
           </li>
-          <li className={`${pathname === "/contact" ? "active" : ""}`}>
+          <li className="rounded-md hover:bg-accent p-0">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,24 +1,39 @@
-import styles from "./Hero.module.css";
-import { BiLogoReact } from "react-icons/bi";
-import { TbBrandNextjs } from "react-icons/tb";
+import Link from "next/link";
+import { BiLinkExternal } from "react-icons/bi";
 import { SiUdacity, SiPluralsight } from "react-icons/si";
 
 function Hero() {
   return (
-    <section className={`flex flex-col py-24`}>
-      <h1 className={`font-extrabold text-5xl md:text-8xl`}>Isaac Awonugba</h1>
-      <p className={`font-semibold text-3xl md:text-5xl`}>Frontend Developer</p>
-      <p className={`text-xl py-16 max-w-4xl`}>
+    <section className="flex flex-col py-24">
+      <h1 className="font-extrabold text-5xl md:text-8xl">Isaac Awonugba</h1>
+      <p className="font-semibold text-3xl md:text-5xl text-accent">
+        Frontend Developer
+      </p>
+      <p className="text-xl py-16 max-w-4xl">
         I am passionate about building accessible, scalable and functional
         applications using Typescript, Nextjs and React.
       </p>
-      <p className={`flex flex-col gap-3 lg:flex-row lg:space-x-16`}>
-        <span className={`flex flex-row items-center space-x-4`}>
-          <SiUdacity className={styles.svg} />
+      <p className="flex flex-col gap-3 md:flex-row md:space-x-16">
+        <span className="flex flex-row items-center space-x-4">
+          <SiUdacity />
           <span>Frontend Web Development</span>
+          <Link
+            href="https://confirm.udacity.com/e/48a2396a-c38c-11ed-ae7e-3b117adf5f13"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <BiLinkExternal className="styled" />
+          </Link>
         </span>
-        <span className={`flex flex-row items-center space-x-4`}>
+        <span className="flex flex-row items-center space-x-4">
           <SiPluralsight /> <span>React Learning sponsored by Meta</span>
+          <Link
+            href="https://app.pluralsight.com/profile/isaac-awonugba-11#0"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <BiLinkExternal className="styled" />
+          </Link>
         </span>
       </p>
     </section>
